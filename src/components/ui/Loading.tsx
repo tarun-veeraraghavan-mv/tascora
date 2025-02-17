012b8e201760f77e1fa6601859fc69a631950766
tarun-veeraraghavan-mv <tarun.veeraraghavan.dev@gmail.com>
2025-02-16 20:08:47 -0700

    loading spinners

---
+++ src/components/ui/Loading.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { CircularProgress } from "@heroui/react";
+import React from "react";
+
+export default function Loading() {
+  return <CircularProgress label="Loading..." />;
+}
